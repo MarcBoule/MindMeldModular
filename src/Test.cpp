773d076b178fd3a8507119c6f149af9b96f3d19d
MarcBoule <marc.boule@etsmtl.ca>
2020-05-12 21:35:52 -0400

    finish 12 db version and add switch for that and solo

--- src/Test.cpp
+++ src/Test.cpp
@@ -24,6 +24,8 @@ struct Test : Module {
 	enum ParamIds {
 		CROSSOVER_PARAM,
 		BASS_WIDTH_PARAM,// 0 to 1.0f; 0 is stereo, 1 is mono
+		TWENTYFOURDB_PARAM,// 24 dB/oct when true, else 12 dB
+		SOLO_PARAM,
 		NUM_PARAMS
 	};
 	
@@ -54,6 +56,7 @@ struct Test : Module {
 	// No need to save, with reset
 	float crossover;
 	dsp::BiquadFilter butter[8];// L low 1, low 2, R low 1, low 2, L high 1, high 2, R high 1, high 2
+	dsp::IIRFilter<2 + 1, 2 + 1, float> onepole[8];
 	dsp::SlewLimiter bassWetSlewer;
 	
 	// No need to save, no reset
@@ -61,12 +64,23 @@ struct Test : Module {
 	
 	
 	void setFilterCutoffs(float nfc) {
+		// butter
 		for (int i = 0; i < 4; i++) { 
-			butter[i].setParameters(dsp::BiquadFilter::LOWPASS_1POLE, nfc, 1.0f / std::sqrt(2.0f), 1.f);
-			butter[i + 4].setParameters(dsp::BiquadFilter::HIGHPASS_1POLE, nfc, 1.0f / std::sqrt(2.0f), 1.f);
+			butter[i].setParameters(dsp::BiquadFilter::LOWPASS, nfc, 1.0f / std::sqrt(2.0f), 1.f);
+			butter[i + 4].setParameters(dsp::BiquadFilter::HIGHPASS, nfc, 1.0f / std::sqrt(2.0f), 1.f);
+		}
+		// one pole
+		float hbcst = 1.0f / (1.0f + M_PI * nfc);
+		float hb[2 + 1] = {hbcst, -hbcst, 0.0f};
+		float acst = (M_PI * nfc - 1.0f) / (M_PI * nfc + 1.0f);
+		float a[2] = {acst, 0.0f};
+		float lbcst = 1.0f - hbcst;
+		float lb[2 + 1] = {lbcst, lbcst, 0.0f};
+		for (int i = 0; i < 4; i++) { 
+			onepole[i].setCoefficients(lb, a);
+			onepole[i + 4].setCoefficients(hb, a);
 		}
 	}
-	
 	
 	
 	Test() {
@@ -74,7 +88,8 @@ struct Test : Module {
 
 		configParam(CROSSOVER_PARAM, 50.0f, 500.0f, 120.0f, "Crossover", " Hz");
 		configParam(BASS_WIDTH_PARAM, 0.0f, 1.0f, 1.0f, "Dry/Wet", "%", 0.0f, 100.0f);// diplay params are: base, mult, offset
-					
+		configParam(TWENTYFOURDB_PARAM, 0.0f, 1.0f, 1.0f, "24 dB/oct");
+		configParam(SOLO_PARAM, 0.0f, 1.0f, 0.0f, "Solo");
 					
 		onReset();
 		
@@ -91,6 +106,7 @@ struct Test : Module {
 		
 		for (int i = 0; i < 8; i++) { 
 			butter[i].reset();
+			onepole[i].reset();
 		}
 		bassWetSlewer.reset();
 	}
@@ -126,12 +142,26 @@ struct Test : Module {
 			setFilterCutoffs(nfc);
 		}
 		
-		float leftLow = butter[0].process(butter[1].process(inputs[IN_INPUTS + 0].getVoltage()));
-		float leftHigh = butter[4].process(butter[5].process(inputs[IN_INPUTS + 0].getVoltage()));
-		float rightLow = butter[2].process(butter[3].process(inputs[IN_INPUTS + 1].getVoltage()));
-		float rightHigh = butter[6].process(butter[7].process(inputs[IN_INPUTS + 1].getVoltage()));
-		
-		
+		float leftLow;
+		float leftHigh;
+		float rightLow;
+		float rightHigh;
+		if (params[TWENTYFOURDB_PARAM].getValue() >= 0.5f) {
+			leftLow = butter[0].process(butter[1].process(inputs[IN_INPUTS + 0].getVoltage()));
+			leftHigh = butter[4].process(butter[5].process(inputs[IN_INPUTS + 0].getVoltage()));
+			rightLow = butter[2].process(butter[3].process(inputs[IN_INPUTS + 1].getVoltage()));
+			rightHigh = butter[6].process(butter[7].process(inputs[IN_INPUTS + 1].getVoltage()));
+		}
+		else {
+			leftLow = onepole[0].process(onepole[1].process(inputs[IN_INPUTS + 0].getVoltage()));
+			leftHigh = onepole[4].process(onepole[5].process(inputs[IN_INPUTS + 0].getVoltage()));
+			rightLow = onepole[2].process(onepole[3].process(inputs[IN_INPUTS + 1].getVoltage()));
+			rightHigh = onepole[6].process(onepole[7].process(inputs[IN_INPUTS + 1].getVoltage()));
+		}
+		// outputs[OUT_OUTPUTS + 0].setVoltage(leftLow);
+		// outputs[OUT_OUTPUTS + 1].setVoltage(leftHigh);
+// return;
+
 		// Bass width (apply to leftLow and rightLow
 		float newBassWidth = params[BASS_WIDTH_PARAM].getValue();
 		if (newBassWidth != bassWetSlewer.out) {
@@ -145,9 +175,13 @@ struct Test : Module {
 		leftLow = leftSig;
 		rightLow = rightSig;
 		
-		
-		outputs[OUT_OUTPUTS + 0].setVoltage(leftLow + leftHigh);
-		outputs[OUT_OUTPUTS + 1].setVoltage(rightLow + rightHigh);
+		bool solo = params[SOLO_PARAM].getValue() >= 0.5f;
+		if (!solo) {
+			leftLow += leftHigh;
+			rightLow += rightHigh;
+		}
+		outputs[OUT_OUTPUTS + 0].setVoltage(leftLow);
+		outputs[OUT_OUTPUTS + 1].setVoltage(rightLow);
 	}// process()
 };
 
@@ -163,6 +197,10 @@ struct TestWidget : ModuleWidget {
 		// Main panel from Inkscape
         setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/dark/meld-1-8.svg")));
  
+		// 24 db switch and solo button
+		addParam(createParamCentered<CKSS>(mm2px(Vec(10.13, 34.5 + 10.85 * 0 - 6)), module, Test::TWENTYFOURDB_PARAM));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(20.15, 34.5 + 10.85 * 0 - 6)), module, Test::SOLO_PARAM));
+
 		// crossover knob
 		addParam(createParamCentered<Davies1900hLargeWhiteKnob>(mm2px(Vec(15.22, 34.5 + 10.85 * 2)), module, Test::CROSSOVER_PARAM));
 		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(15.22, 34.5 + 10.85 * 4)), module, Test::BASS_WIDTH_PARAM));
