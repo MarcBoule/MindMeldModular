a27cb2a549ea60c36bd03bf3926b15282de076ec
MarcBoule <marc.boule@etsmtl.ca>
2021-12-12 12:03:31 -0500

    finish eq menu re-code

--- src/EqMaster/EqMenus.hpp
+++ src/EqMaster/EqMenus.hpp
@@ -56,33 +56,20 @@ struct CvLevelSlider : ui::Slider {
 struct KnobArcShowItem : MenuItem {
 	int8_t *srcDetailsShow;
 
-	struct KnobArcShowSubItem : MenuItem {
-		int8_t *srcDetailsShow;
-		int setVal;
-		void onAction(const event::Action &e) override {
-			*srcDetailsShow &= ~0x3;
-			*srcDetailsShow |= setVal;
-		}
-	};
-	
 	Menu *createChildMenu() override {
 		Menu *menu = new Menu;
-
-		KnobArcShowSubItem *arcShowItem0 = createMenuItem<KnobArcShowSubItem>("On", CHECKMARK((*srcDetailsShow & 0x3) == 0x3));
-		arcShowItem0->srcDetailsShow = srcDetailsShow;
-		arcShowItem0->setVal = 0x3;
-		menu->addChild(arcShowItem0);
-		
-		KnobArcShowSubItem *arcShowItem1 = createMenuItem<KnobArcShowSubItem>("CV only", CHECKMARK((*srcDetailsShow & 0x3) == 0x2));
-		arcShowItem1->srcDetailsShow = srcDetailsShow;
-		arcShowItem1->setVal = 0x2;
-		menu->addChild(arcShowItem1);
-		
-		KnobArcShowSubItem *arcShowItem2 = createMenuItem<KnobArcShowSubItem>("Off", CHECKMARK((*srcDetailsShow & 0x3) == 0x0));
-		arcShowItem2->srcDetailsShow = srcDetailsShow;
-		arcShowItem2->setVal = 0x0;
-		menu->addChild(arcShowItem2);
-		
+		menu->addChild(createCheckMenuItem("On", "",
+			[=]() {return (*srcDetailsShow & 0x3) == 0x3;},
+			[=]() {*srcDetailsShow &= ~0x3;   *srcDetailsShow |= 0x3;}
+		));	
+		menu->addChild(createCheckMenuItem("CV only", "",
+			[=]() {return (*srcDetailsShow & 0x3) == 0x2;},
+			[=]() {*srcDetailsShow &= ~0x3;   *srcDetailsShow |= 0x2;}
+		));	
+		menu->addChild(createCheckMenuItem("Off", "",
+			[=]() {return (*srcDetailsShow & 0x3) == 0x0;},
+			[=]() {*srcDetailsShow &= ~0x3;   *srcDetailsShow |= 0x0;}
+		));	
 		return menu;
 	}
 };
@@ -90,24 +77,16 @@ struct KnobArcShowItem : MenuItem {
 struct MomentaryCvItem : MenuItem {
 	int8_t *momentaryCvButtonsSrc;
 
-	struct MomentaryCvSubItem : MenuItem {
-		int8_t *momentaryCvButtonsSrc;
-		void onAction(const event::Action &e) override {
-			*momentaryCvButtonsSrc ^= 0x1;
-		}
-	};
-
 	Menu *createChildMenu() override {
 		Menu *menu = new Menu;
-
-		MomentaryCvSubItem *mo1Item = createMenuItem<MomentaryCvSubItem>("Trigger toggle", CHECKMARK(*momentaryCvButtonsSrc == 1));
-		mo1Item->momentaryCvButtonsSrc = momentaryCvButtonsSrc;
-		menu->addChild(mo1Item);
-
-		MomentaryCvSubItem *mo0Item = createMenuItem<MomentaryCvSubItem>("Gate high/low", CHECKMARK(*momentaryCvButtonsSrc == 0));
-		mo0Item->momentaryCvButtonsSrc = momentaryCvButtonsSrc;
-		menu->addChild(mo0Item);
-
+		menu->addChild(createCheckMenuItem("Trigger toggle", "",
+			[=]() {return *momentaryCvButtonsSrc == 1;},
+			[=]() {*momentaryCvButtonsSrc ^= 0x1;}
+		));	
+		menu->addChild(createCheckMenuItem("Gate high/low", "",
+			[=]() {return *momentaryCvButtonsSrc == 0;},
+			[=]() {*momentaryCvButtonsSrc ^= 0x1;}
+		));	
 		return menu;
 	}
 };
@@ -117,21 +96,13 @@ struct MomentaryCvItem : MenuItem {
 struct FetchLabelsItem : MenuItem {
 	int64_t *mappedIdSrc;
 	
-	struct FetchLabelsSubItem : MenuItem {
-		int64_t *mappedIdSrc;
-		int64_t setId;
-		void onAction(const event::Action &e) override {
-			*mappedIdSrc = setId;
-		}
-	};
-	
 	Menu *createChildMenu() override {
 		Menu *menu = new Menu;
 
-		FetchLabelsSubItem *id0Item = createMenuItem<FetchLabelsSubItem>("None (default labels)", CHECKMARK(*mappedIdSrc == 0));
-		id0Item->mappedIdSrc = mappedIdSrc;
-		id0Item->setId = 0;
-		menu->addChild(id0Item);
+		menu->addChild(createCheckMenuItem("None (default labels)", "",
+			[=]() {return *mappedIdSrc == 0;},
+			[=]() {*mappedIdSrc = 0;}
+		));	
 
 		bool sawMappedId = *mappedIdSrc == 0;
 		
@@ -141,20 +112,21 @@ struct FetchLabelsItem : MenuItem {
 				sawMappedId = true;
 			}
 			std::string mixerName = std::string(pl.name) + string::f("  (id %" PRId64 ")", pl.id);
-			FetchLabelsSubItem *idItem = createMenuItem<FetchLabelsSubItem>(mixerName, CHECKMARK(*mappedIdSrc == pl.id));
-			idItem->mappedIdSrc = mappedIdSrc;
-			idItem->setId = pl.id;
-			menu->addChild(idItem);
+			menu->addChild(createCheckMenuItem(mixerName, "",
+				[=]() {return *mappedIdSrc == pl.id;},
+				[=]() {*mappedIdSrc = pl.id;}
+			));	
 		}
 		delete mixerMessageSurvey;
 		
 		if (!sawMappedId) {
+			int64_t deletedMappedIdSrc = *mappedIdSrc;
 			std::string mixerName = std::string("[deleted]") + string::f("  (id %" PRId64 ")", *mappedIdSrc);
-			FetchLabelsSubItem *idItem = createMenuItem<FetchLabelsSubItem>(mixerName, CHECKMARK(true));
-			idItem->mappedIdSrc = mappedIdSrc;
-			idItem->setId = *mappedIdSrc;
-			idItem->disabled = true;
-			menu->addChild(idItem);
+			menu->addChild(createCheckMenuItem(mixerName, "",
+				[=]() {return *mappedIdSrc == deletedMappedIdSrc;},
+				[=]() {},
+				true
+			));	
 		}
 		
 		return menu;
@@ -185,14 +157,6 @@ struct CopyTrackSettingsItem : MenuItem {
 struct DecayRateItem : MenuItem {
 	int8_t *decayRateSrc;
 
-	struct DecayRateSubItem : MenuItem {
-		int8_t *decayRateSrc;
-		int8_t setVal;
-		void onAction(const event::Action &e) override {
-			*decayRateSrc = setVal;
-		}
-	};
-
 	Menu *createChildMenu() override {
 		Menu *menu = new Menu;
 
@@ -204,10 +168,10 @@ struct DecayRateItem : MenuItem {
 		};
 		
 		for (int i = 0; i < 4; i++) {
-			DecayRateSubItem *drItem = createMenuItem<DecayRateSubItem>(decayRateNames[i], CHECKMARK(*decayRateSrc == i));
-			drItem->decayRateSrc = decayRateSrc;
-			drItem->setVal = i;
-			menu->addChild(drItem);
+			menu->addChild(createCheckMenuItem(decayRateNames[i], "",
+				[=]() {return *decayRateSrc == i;},
+				[=]() {*decayRateSrc = i;}
+			));	
 		}
 		
 		return menu;
